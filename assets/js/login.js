8996922472902a72535fb2668b46957991688b0f
cloneJK <798287855@qq.com>
2022-10-17 09:53:17 +0800

    login

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,7 +13,7 @@ $(function () {
     })
 
     var form = layui.form
-    var layer=layui.layer
+    var layer = layui.layer
     form.verify({
         // 自定义一个叫做pwd的校验规则 第一个是规则 第二个是不满足规则透出的内容
         'pwd': [
@@ -31,26 +31,52 @@ $(function () {
 
     })
 
-
+    //http://www.liulongbin.top:3007
 
     // 注册提交事件
     $('#form_reg').on('submit', function (e) {
         // console.log(e);
         // 阻拦表单默认提交行为
         e.preventDefault();
-        $.post('http://www.liulongbin.top:3007/api/reguser', { username: $('#form_reg [name=username]').val(),
-         password: $('#form_reg [name=password]').val() }, function (res) {
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('http://www.liulongbin.top:3007/api/reguser', data, function (res) {
             console.log(res);
-            if(res.status !==0){
+            if (res.status !== 0) {
                 // return console.log(res.message)
                 return layer.msg(res.message)
             }
             layer.msg('注册成功 请登录')
+
+            $('#link_login').click()
         })
 
     })
 
+    // 登录提交事件
+    //qqwer123  123456
+   // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mjk0NjcsInVzZXJuYW1lIjoicXF3ZXIxMjMiLCJwYXNzd29yZCI6IiIsIm5pY2tuYW1lIjoiIiwiZW1haWwiOiIiLCJ1c2VyX3BpYyI6IiIsImlhdCI6MTY2NTk3MDY3NiwiZXhwIjoxNjY2MDA2Njc2fQ.6UB7jOCSp8c4F2dGFu5y1LkZF4H059EORQUcD_HYSXA
+    $('#form_login').submit(function (e) {
+        e.preventDefault()
+        $.ajax({
+            url: 'http://www.liulongbin.top:3007/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('登录失败')
+                }
+                layer.msg('登录成功')
+                console.log(res.token);
+                localStorage.setItem('token',res.token)
+                location.href = '/index.html'
+            }
+        })
 
+    })
 
 
 
