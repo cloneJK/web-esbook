821ad845b61253d10b2efc3e24bfb58d9159403b
cloneJK <798287855@qq.com>
2022-10-17 13:41:22 +0800

    主页没有token不允许登录

--- assets/js/baseAPi.js
+++ assets/js/baseAPi.js
@@ -1,9 +1,36 @@
 // $.ajaxPrefilter
 //每次调用$.ajax()发起请求的函数都会先调用下这个函数
 //在这个函数中就能拿到我们给ajax提供的配置对象
-$.ajaxPrefilter(function(options){
-    
+$.ajaxPrefilter(function (options) {
+
     //再发起真正的ajax请求之前 统一拼接
-    options.url=`http://www.liulongbin.top:3007`+options.url
+    options.url = `http://www.liulongbin.top:3007` + options.url
     console.log(options.url);
+
+
+
+
+
+    //统一为有权限的接口 设置headers请求头
+    //判断 如果请求的地址里面 有my那就是要请求头的 不是 就不用请求头
+    if (options.url.indexOf('/my/') !== -1) {
+
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //全家挂载complete
+    options.complete=function(res){
+        //在complete回调函数里面可以使用res.responseJSON拿到服务器响应回来的数据
+        // console.log(res.responseJSON);
+
+        if(res.responseJSON.status===1&&res.responseJSON.message==='身份认证失败！'){
+            localStorage.removeItem('token')
+            location.href='/login.html'
+        }
+    }
+
 })
+
+
---
+++ assets/js/index.js
@@ -0,0 +1,67 @@
+$(function () {
+
+
+
+
+    //获取用户基本信息
+    getUserInfo()
+    var layer=layui.layer
+    $('#btnLogout').on('click',function(){
+        console.log('ok');
+        //提示用户是否要登录
+        layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            localStorage.removeItem('token')
+            location.href='/login.html'
+
+            layer.close(index);
+          });
+    })
+
+
+})
+
+
+//获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        
+        success: function (res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            // layui.layer.msg('获取用户信息成功')
+            //渲染用户头像
+            rederAvater(res.data)
+        },
+        //不管成功还是失败都会调用complete
+   
+    })
+}
+
+function rederAvater(user) {
+    //如果用户有nickname就使用不然用username做名称
+    var name=user.nickname||user.username
+    //拼接欢迎词
+    $('#welcome').html('欢迎 &nbsp;&nbsp;'+name)
+    //设置头像 如果有图片头像就显示图片头像让文字头像隐藏
+    if(user.user_pic!==null){
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-avatar').hide()
+    }else{
+        //如果有文字头像就隐藏图片头像
+        $('.layui-nav-img').attr('src',user.user_pic).hide()
+        //取名称第一个字母并作大写
+        var first=name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+
+    }
+
+
+
+
+
+}
